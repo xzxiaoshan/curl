2165cc0e508c23f903c9bea2cf4969c7a1165c9f
shanhy <xz.xiaoshan@gmail.com>
2023-08-15 10:55:07 +0800

    配置调整

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,6 +1,6 @@
 package org.toilelibre.libe.curl;
 
 public final class Version {
-    public static String NUMBER = "1.0.0-SNAPSHOT";
-    public static String BUILD_TIME = "2023-07-26T12:26:59Z";
+    public static String NUMBER = "0.0.41-SNAPSHOT";
+    public static String BUILD_TIME = "2023-08-15 10:54:16";
 }
